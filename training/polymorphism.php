f5f1e02327e2675554242974230e5bad272b3903
Leandro de Paula <leandrodepaula.ti@gmail.com>
2023-02-16 05:39:35 -0300

    Update polymorphism

--- training/polymorphism.php
+++ training/polymorphism.php
@@ -7,14 +7,19 @@ class Animal
     public function walk() {
         echo "The animal walked";
     }
+
+    public function run() {
+        echo "The animal ran";
+    }
 }
 
 class Horse extends Animal
 {
-    public function walkTwo(){
-        echo "The horse worked";
+    public function walk(){
+        echo "The horse worked <br>";
+        $this->run();
     }
 }
 
 $animal = new Horse();
-echo $animal->walk();
+$animal->walk();
