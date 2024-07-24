9da0a8735d308be106e73f7c59162b3d4169eaa0
leandro-de-paula <leandrodepaula.ti@gmailcom>
2024-07-24 07:04:08 -0300

    Reviewed Do While

---
+++ exercises/doWhile.php
@@ -0,0 +1,13 @@
+<?php
+$i = 0;
+do {
+    echo "Testing Do While Increment: $i \n";
+    $i++;
+} while ($i <= 10);
+
+$a = 10;
+do {
+    echo "testing Do While Decrement: $a \n";
+    $a--;
+} while ($a >= 0);
+
